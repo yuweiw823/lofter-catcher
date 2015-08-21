0f93c561d1536112568d065401e77b00b00972f0
yuwei wang <yuweiw823@gmail.com>
2015-08-21 00:18:33 -0700

    done

--- Lofter抓文插件/contentScript.js
+++ Lofter抓文插件/contentScript.js
@@ -1,38 +1,37 @@
 //This is the content script
-// http://verdancy.lofter.com/view
-
-var totalItemLength = parseInt(document.querySelector('.m-txtsch > .txt > .currt > span').innerText);
-
 window.onload = function () {
+	var totalItem = document.querySelector('.m-txtsch > .txt > .currt > span');
+	totalItemLength = totalItem ? parseInt(totalItem.innerText) : 0 ;
 	console.log('ready');
+	var startURL = document.URL;
+
 	chrome.runtime.onConnect.addListener (function (port) {
 		console.log(port.name == "lofterCatcher");
 		port.onMessage.addListener(function (msg) {
-			if (msg == "click") {
-
-				var startURL = document.URL;
-				if ((/http\:\/\/[a-z0-9\-]{5,}\.lofter\.com\/view/).test(startURL)) {
-					port.postMessage('rightStartURL');
-					var timer = setInterval(function(){
-						port.postMessage('pageLoading');
-						window.scrollBy(0, 1000);
-						var loadedItemLength = document.getElementsByClassName('g-bdc')[0].querySelectorAll(".text, .img, .music, .movie").length;
-						if(loadedItemLength == totalItemLength){
-							clearInterval(timer);
-							port.postMessage('pageLoaded');
-
-							chrome.storage.sync.get(function (data){
-								var keyword = data.keyword;
-								port.postMessage('catching');
-								getAllLinks(keyword);
-							});
-						}
-					}, 500);
-				} else {
-					port.postMessage('wrongStartURL');
-				}
+			if ((/http\:\/\/[a-z0-9\-]{5,}\.lofter\.com\/view/).test(startURL) == false) {
+				port.postMessage('wrongStartURL');
 			} else {
-				port.postMessage('error');
+				try {
+					port.postMessage('rightStartURL');
+					if (msg == "click") {
+						var timer = setInterval(function(){
+							port.postMessage('pageLoading');
+							window.scrollBy(0, 1000);
+							var loadedItemLength = document.getElementsByClassName('g-bdc')[0].querySelectorAll(".text, .img, .music, .movie").length;
+							if(loadedItemLength == totalItemLength){
+								clearInterval(timer);
+								port.postMessage('pageLoaded');
+								chrome.storage.sync.get(function (data){
+									var keyword = data.keyword;
+									port.postMessage('catching');
+									getAllLinks(keyword);
+								});
+							}
+						}, 500);
+					}
+				} catch (err) {
+					port.postMessage('error');
+				}					
 			}
 		});
 	});
@@ -45,7 +44,6 @@ function getAllLinks(_keyword) {
 	var processItem = [];
 	var output = '';
 
-
 	for (var i=0; i<allArticleList.length; i++){
 		if(allArticleList[i].innerText.indexOf(_keyword) > -1){
 			articleUrlList.push(allArticleList[i].parentNode.getAttribute('href'));
@@ -53,9 +51,9 @@ function getAllLinks(_keyword) {
 		}
 	};
 	var n = articleUrlList.length;
-	var outputWindow = window.open('','targetWindow','scrollbars=0,resizable=1,width=800,height=600');
+	var outputWindow = window.open('','targetWindow','scrollbars=0,resizable=1,width=600,height=450');
+	outputWindow.focus();
 	getOutPut(articleUrlList, articleTitleList, processItem, n-1, output, outputWindow);
-	
 }
 
 function getOutPut(articleUrlList, articleTitleList, processItem, k, output, outputWindow) {
@@ -84,27 +82,29 @@ function getOutPut(articleUrlList, articleTitleList, processItem, k, output, out
 	
 	outputWindow.document.title = '抓取结果';
 	outputWindow.document.body.innerHTML = (k<=0) ? 
-		('<h3 id="outputMessage" style='+redStyle+'>抓取完毕</h3><button id="copybtn" style=' + buttonStyle + '>复制文本</button><textarea id="copyarea" style="width:100%; height:88%">'+ output + '</textarea>') 
+		('<h3 id="outputMessage" style='+redStyle+'>抓取完毕</h3><button id="copybtn" style=' + buttonStyle + '>复制文本</button><textarea id="copyarea" style="width:100%; height:85%">'+ output + '</textarea>') 
 		: '<h3 style='+redStyle+'>努力抓取中…… 正在抓取第 ' + (len-k) + ' 篇，共 ' + len + ' 篇</h3>';
-	if(k<=0){
+	if(k<0){
+		if (output == ''){
+			outputWindow.document.body.innerHTML = '<h3 id="outputMessage" style='+redStyle+'>抓取失败，未找到相应结果</h3>'
+		}
 		copyOutput(outputWindow);
 	};
 	return output;
 }
 
-// var authorInfo = "\n\n*******Lofter 抓文插件 Powered by http://afanvera.lofter.com*******";
 var redStyle = "\"margin:5px; color:#BA0808; text-align:center; font-family:\'Hiragino Sans GB\', \'Microsoft YaHei\', 微软雅黑, tahoma, arial, simsun, 宋体;\"";
-var buttonStyle = "\"width:20%; height:26px; margin:2px 40% 6px; padding-bottom: 4px; background-color: #DFDFDF; font-family:\'Hiragino Sans GB\', \'Microsoft YaHei\', 微软雅黑, tahoma, arial, simsun, 宋体;\""
+var buttonStyle = "\"width:20%; height:26px; margin:2px 40% 8px; padding-bottom: 4px; background-color: #DFDFDF; font-family:\'Hiragino Sans GB\', \'Microsoft YaHei\', 微软雅黑, tahoma, arial, simsun, 宋体;\""
+
 function copyOutput(outputWindow){
 	var outputMessage = outputWindow.document.getElementById('outputMessage');
 	var copyTextareaBtn = outputWindow.document.getElementById('copybtn');
 	copyTextareaBtn.addEventListener('click', function(event) {
 		var copyTextarea = outputWindow.document.getElementById('copyarea');
 		copyTextarea.select();
-
 		try {
 			var successful = outputWindow.document.execCommand('copy');
-			outputMessage.innerHTML = successful ? '成功复制到剪贴板！' : '未能复制成功，可点击按钮【全选文本】，右键手动复制。';
+			outputMessage.innerHTML = successful ? '复制成功！' : '复制失败。可点击按钮【全选文本】，右键手动复制。';
 			copyTextareaBtn.innerText = successful ? '复制文本' : '全选文本';
 		} catch (err) {
 			outputMessage.innerHTML = '未能复制成功，可点击按钮【全选文本】，右键手动复制。';
--- Lofter抓文插件/popup.js
+++ Lofter抓文插件/popup.js
@@ -6,9 +6,10 @@ window.onload = function () {
   catcherButton.onclick = function (){
     var keyWord = document.getElementById('title_keyword').value;
     if(keyWord == '' || keyWord.length == 0) {
-      msgDiv.innerHTML += '<p>关键词为空，无法抓取！</p>';
+      msgDiv.innerHTML += '<p>关键词为空，无法抓取！</br> </p>';
       return false;
     } else {
+      msgDiv.innerHTML = '<p>Lofter网页加载速度较慢，请稍等一下再点击抓取按钮...<br/>若长时间未能响应，请刷新归档页，重新抓取。</p>';
       chrome.storage.sync.set({'keyword': keyWord});
       
       chrome.tabs.query({active: true, currentWindow: true}, function (tabs){
@@ -18,31 +19,41 @@ window.onload = function () {
 
           switch (msg) {
             case 'rightStartURL':
-              msgDiv.innerHTML += '<p>正确的归档页，准备抓取...</p>';
+              msgDiv.innerHTML = '<p>正确的归档页，准备抓取...</br> </p>';
               catcherButton.disabled = true;
             break;
             case 'pageLoading':
-              msgDiv.innerHTML += '<p>归档页加载中...</p>';
+              catchCount++;
+              if(catchCount>10){
+                if(catchCount==30) {
+                  msgDiv.innerHTML += '<p>页面加载有点慢，请稍等...  <br/>若长时间未能响应，请刷新归档页重新抓取</p>';
+                }
+                msgDiv.innerHTML += '';             
+              } else {
+                msgDiv.innerHTML += '<p>归档页加载中...</p>';
+              }
             break;
             case 'pageLoaded':
-              msgDiv.innerHTML += '<p>归档页加载完毕...</p>';
+              msgDiv.innerHTML += '<p>归档页加载完毕....</p>';
             break;
             case 'catching':
-              msgDiv.innerHTML += '<p>抓取中...</p>';
+              msgDiv.innerHTML += '<p> </br>抓取结果将显示在弹出窗口...</p>';
             break;
-            case 'done':
-              msgDiv.innerHTML += '<p>抓取完毕 ~\\(≧▽≦)/~</p>';
-            break;
+            // case 'done':
+            //   msgDiv.innerHTML += '<p>抓取完毕 ~\\(≧▽≦)/~</p>';
+            // break;
+            // case 'noKeyword':
+            //   msgDiv.innerHTML += '<p>未能找到关键词</p>';
+            // break;
             case 'wrongStartURL':
-              msgDiv.innerHTML += '<p>当前页面非lofter博主的归档页，请在归档页使用本插件</p>';
+              msgDiv.innerHTML = '<p>当前页面非lofter博主的归档页，请在归档页使用本插件</br> </p>';
             break;
             default:
-              msgDiv.innerHTML += '<p>出错了T^T，请刷新归档页再试一次~</p>';
-
+              msgDiv.innerHTML = '<p>出错了T^T，请刷新归档页再试一次~</br> </p>';
           }
         });
       });
-    }
 
+    }
   };
 };
